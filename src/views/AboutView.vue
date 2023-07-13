d4be194982594030f95e52187713525e1cffed2d
Nickholas <s10843037@ems.tcust.edu.tw>
2023-07-14 00:58:50 +0800

    code update

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,7 +1,8 @@
 <template>
-  <div class="about pa-6">
+  <div class="about pa-6 text-center">
     <h2>About this To Do List App</h2>
+    <br>
     <p>
-      This To Do List Application was made for the purpose of familiarrize myself about Vue.JS and Vuetify framework.</p>
+      This To Do List Application was made for the purpose of familiarize myself about Vue.JS and Vuetify framework.</p>
   </div>
 </template>
--- src/views/ToDoView.vue
+++ src/views/ToDoView.vue
@@ -22,6 +22,13 @@
                 <v-icon color="primary lighten-1">mdi-delete-outline</v-icon>
               </v-btn>
             </v-list-item-action>
+
+            <!-- New v-list-item-action for editing task -->
+            <v-list-item-action>
+              <v-btn @click.stop="editTask(task.id)" icon>
+                <v-icon color="primary lighten-1">mdi-pencil</v-icon>
+              </v-btn>
+            </v-list-item-action>
           </template>
         </v-list-item>
         <v-divider></v-divider>
@@ -29,42 +36,77 @@
     </v-list>
 
     <div v-else class="pa-6 text-center">
-    <v-icon large color="grey">mdi-set-none</v-icon>
-    <div class="grey--text">There is no task here.</div>
-  </div>
+      <v-icon large color="grey">mdi-set-none</v-icon>
+      <div class="grey--text">There is no task here.</div>
+    </div>
+
+    <!-- Snackbar for empty task title -->
+    <!-- Snackbar for empty task title -->
+  <v-snackbar
+    v-model="snackbar.show"
+    :timeout="snackbar.timeout"
+    color="error"
+    @input="saveSnackbarState"
+  >
+    {{ snackbar.message }}
+  </v-snackbar>
   </div>
 </template>
 
-
 <script>
-  export default {
-    name: 'Home',
-    data(){
-      return{
-        newTaskTitle:'',
-        tasks:[
-        ]
-      }
-    },
-    methods:{
-      doneTask(id){
-        let task = this.tasks.filter(task => task.id===id)[0]
-        task.done = !task.done
+export default {
+  name: 'Home',
+  data() {
+    return {
+      newTaskTitle: '',
+      tasks: [],
+      snackbar: {
+        show: false,
+        timeout: 3000,
+        message: 'Unable to add empty task',
       },
-
-      deleteTask(id) {
-      this.tasks = this.tasks.filter(task => task.id !== id)
+    };
+  },
+  methods: {
+    doneTask(id) {
+      let task = this.tasks.find((task) => task.id === id);
+      task.done = !task.done;
     },
-
-    addTask(){
-      let newTask={
-        id:Date.now(),
-        title:this.newTaskTitle,
-        done:false
+    deleteTask(id) {
+      this.tasks = this.tasks.filter((task) => task.id !== id);
+    },
+    editTask(id) {
+      let task = this.tasks.find((task) => task.id === id);
+      let newTitle = prompt('Enter new task title', task.title);
+      if (newTitle !== null) {
+        task.title = newTitle.trim();
       }
-      this.tasks.push(newTask)
-      this.newTaskTitle=''
-    }
-    }
-  }
+    },
+    addTask() {
+      if (this.newTaskTitle.trim() === '') {
+        this.snackbar.show = true;
+        return;
+      }
+      let newTask = {
+        id: Date.now(),
+        title: this.newTaskTitle,
+        done: false,
+      };
+      this.tasks.push(newTask);
+      this.newTaskTitle = '';
+    },
+    saveSnackbarState() {
+      localStorage.setItem('snackbarState', JSON.stringify(this.snackbar.show));
+    },
+    retrieveSnackbarState() {
+      const state = localStorage.getItem('snackbarState');
+      if (state !== null) {
+        this.snackbar.show = JSON.parse(state);
+      }
+    },
+  },
+  created() {
+    this.retrieveSnackbarState();
+  },
+};
 </script>
